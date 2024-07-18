f2d2f6feb257063e971fcebae7bc1c4cd490de64
Schirni <christoph.schirninger@edu.uni-graz.at>
2024-07-18 17:58:37 +0000

    fix save to fits files Co-authored-by: Andres Muñoz-Jaramillo <amunozj@users.noreply.github.com> Co-authored-by: marmargarida <marmargarida@users.noreply.github.com> Co-authored-by: Brianna N. Isola <briannaisola@users.noreply.github.com>

--- sunerf/evaluation/image_render.py
+++ sunerf/evaluation/image_render.py
@@ -50,7 +50,7 @@ class ImageRender:
             plt.imsave(img_path, image, cmap=cmap, vmin=0, vmax=1)
 
 
-    def save_frame_as_fits(self, i, point, model_outputs, wavelength, half_fov=1.3, itype='imager', obs_date='2014-04-01T00:00:00.000'):
+    def save_frame_as_fits(self, i, point, model_output, wavelength, half_fov=1.3, itype='imager', obs_date='2014-04-01T00:00:00.000'):
         r"""Method that saves an image from a viewpoint as the ith frame as fits file
         Args
         ------
@@ -67,17 +67,15 @@ class ImageRender:
         # save result image
         output_path =  f"{self.render_path}/{itype}/{wavelength}"
         os.makedirs(output_path, exist_ok=True)
-        img_path = f'{output_path}/{str(i).zfill(3)}_w{wavelength}_lat{np.round(lat,1)}_lon{np.round(lon,1)}_r{np.round(d,2)}_T{np.round(time,2)}.fits'
+        img_path = f'{output_path}/{str(i).zfill(3)}_w{wavelength}_lat{np.round(lat,1)}_lon{np.round(lon,1)}_r{np.round(d,2)}_T{(time.strftime("%Y%m%d-%H%M"))}.fits'
 
         if not os.path.exists(img_path):
 
-             # Undo normalization
-            predicted = outputs['pixel_intensity']
 
             # Create new header
             new_observer = SkyCoord(-lon*u.deg, lat*u.deg, d*u.AU, obstime=obs_date, frame='heliographic_stonyhurst')
 
-            out_shape = predicted.shape
+            out_shape = model_output.shape
             out_ref_coord = SkyCoord(0*u.arcsec, 0*u.arcsec, obstime=new_observer.obstime,
                                     frame='helioprojective', observer=new_observer,
                                     rsun=696000*u.km)
@@ -98,7 +96,7 @@ class ImageRender:
             out_header['r_sun'] = out_shape[0]/2/half_fov
 
             # create dummy sunpy map
-            s_map = Map(predicted, out_header)
+            s_map = Map(model_output, out_header)
             s_map.save(img_path, overwrite=True)
 
 
@@ -148,12 +146,12 @@ if __name__ == '__main__':
                 np.ones(n_points),
                 [avg_time] * n_points)
 
-    points_2 = zip(np.linspace(0, 360, n_points),
-                np.ones(n_points) * 0,
+    points_2 = zip(np.ones(n_points) * 0,
+                np.linspace(0, 360, n_points),
                 np.ones(n_points),
                 [avg_time] * n_points)
 
-    points_3 = zip(np.linspace(0, 180, n_points),
+    points_3 = zip(np.linspace(-90, 90, n_points),
                 np.linspace(0, 360, n_points),
                 np.linspace(1, 0.2, n_points),
                 [avg_time] * n_points )
@@ -164,11 +162,12 @@ if __name__ == '__main__':
     for i, (lat, lon, d, time) in tqdm(list(enumerate(points)), total=len(points)):
         outputs = loader.load_observer_image(lat * u.deg, lon * u.deg, time, distance=d * u.AU, batch_size=batch_size, resolution=resolution)
 
-        for i, wavelength in enumerate(wavelengths):
+        for n, wavelength in enumerate(wavelengths):
             if output_format == 'jpg':
-                render.save_frame_as_jpg(i, outputs['image'][:,:,i], wavelength)
+                render.save_frame_as_jpg(i, outputs['image'][:,:,n], wavelength)
 
             elif output_format == 'fits':
-                pass
+                # i, point, model_output, wavelength,
+                render.save_frame_as_fits(i, (lat, lon, d, time), outputs['image'][:,:,n], wavelength, obs_date=avg_time)
             else:
                 print('No valid format selected')
