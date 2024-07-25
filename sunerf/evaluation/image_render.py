43a70d562f4fad3b5948a6713e3cce72eae27ae0
Marcella <marcella_engcomp@yahoo.com.br>
2024-07-25 22:04:15 +0000

    Working on rendering on MHD images from different observer points. Co-authored-by: Benoit <tremblaybenoit@users.noreply.github.com> Co-authored-by: Rituparna Curt <rituparna50@users.noreply.github.com> Co-authored-by: Brianna N. Isola <briannaisola@users.noreply.github.com>

--- sunerf/evaluation/image_render.py
+++ sunerf/evaluation/image_render.py
@@ -6,7 +6,7 @@ from astropy.coordinates import SkyCoord
 import matplotlib.pyplot as plt
 import numpy as np
 import sunpy.visualization.colormaps as cm
-from sunpy.map import Map, make_fitswcs_header
+from sunpy.map import Map, make_fitswcs_header, all_coordinates_from_map
 from tqdm import tqdm
 # For density and temperature rendering
 from sunerf.rendering.density_temperature import DensityTemperatureRadiativeTransfer
@@ -59,11 +59,20 @@ class ImageRender:
         # Save image if it doesn't exist
         # if not os.path.exists(img_path):
         # Normalize the image
-        image = model_output/np.nanmean(model_output)  # TODO: Consider normalizing over full time sequence
+        image = model_output  # /np.nanmean(model_output)  # TODO: Consider normalizing over full time sequence
         # Get the colormap
         cmap = plt.get_cmap(f'sdoaia{wavelength}').copy()
         # Save the image
-        plt.imsave(img_path, image, cmap=cmap, vmin=0, vmax=np.nanmax(image))
+        # plt.imsave(img_path, image, cmap=cmap, vmin=0, vmax=np.nanmax(image))
+        fig_sizex = 4
+        fig_sizey = 4
+        fig = plt.figure(figsize=(fig_sizex, fig_sizey), constrained_layout = False)
+        ax = fig.add_subplot()
+        ax.imshow(image, cmap=cmap, norm='asinh', vmin=0, vmax=0.9*np.nanmax(image))
+        ax.set_axis_off()
+        plt.draw()
+        plt.savefig(img_path, format='jpeg', dpi=300)
+
 
     def save_frame_as_fits(self, i, point, model_output, wavelength, half_fov=1.3,
                            itype='imager', obs_date='2014-04-01T00:00:00.000'):
@@ -160,7 +169,7 @@ def parse_args():
     p = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     p.add_argument('--render_path', type=str)
-    p.add_argument('--resolution', type=int, default=512) 
+    p.add_argument('--resolution', type=int, default=1024) 
     p.add_argument('--batch_size', type=int, default=4096)
     p.add_argument('--output_format', type=str, default='jpg')
     p.add_argument('--model', type=str, default='SimpleStar' )
@@ -175,6 +184,15 @@ def parse_args():
     args = p.parse_args()
     return args
 
+# def get_positions(cordinates, n_points):
+
+#     # points_1 = zip(np.linspace(-25, 25, n_points),
+#     #         np.linspace(0, 45, n_points),
+#     #         np.linspace(1.5, 0.8, n_points),
+#     #         np.linspace(0, 1, n_points))
+#     time_array = np.linspace(0, 1, n_points)
+#     points = zip(time_array)
+#     return points
 
 if __name__ == '__main__':
     """ Main function to render images from a given model output
@@ -210,8 +228,12 @@ if __name__ == '__main__':
     model = args.model
 
     # Path to AIA files
+    # Retrieving wavelength 171 Angstrom
     path_to_aia_file = "/mnt/disks/data/AIA/171/*.fits"
     s_map = Map(path_to_aia_file)
+    # solar_radius = s_map.meta['distance']
+    # solar_radius = all_coordinates_from_map(smap)
+    
 
     # Initialization of the density and temperature model (Simple star analytical model or MHD model)
     # initialization of density and temperature with simple star 
@@ -236,34 +258,39 @@ if __name__ == '__main__':
     #       (1) Simple star, 
     #       (2) PSI simulation data (might be easier to match the times of the cubes)
  
-    # Generate coordinates for the observer
+    # Generate coordinates for the observer in units [solar rad.]
     # Number of points to generate
-    n_points = 2  # 60
+    n_points = 8  # 60
+    
+    # points_1 = zip(np.linspace(-25, 25, n_points),
+    #             np.linspace(0, 45, n_points),
+    #             np.linspace(1.5, 0.8, n_points),
+    #             np.linspace(0, 1, n_points))
 
-    points_1 = zip(np.linspace(-25, 25, n_points),
-                np.linspace(0, 45, n_points),
-                np.linspace(1.5, 0.8, n_points),
-                np.linspace(0, 1, n_points))
+    # points_2 = zip(np.ones(n_points) * 0,
+    #             np.linspace(0, 360, n_points),
+    #             np.ones(n_points),
+    #             np.linspace(0, 1, n_points))
 
-    points_2 = zip(np.ones(n_points) * 0,
-                np.linspace(0, 360, n_points),
-                np.ones(n_points),
-                np.linspace(0, 1, n_points))
+    # points_3 = zip(np.linspace(-90, 90, n_points),
+    #             np.linspace(0, 360, n_points),
+    #             np.linspace(1, 0.2, n_points),
+    #             np.linspace(0, 1, n_points))
 
-    points_3 = zip(np.linspace(-90, 90, n_points),
-                np.linspace(0, 360, n_points),
-                np.linspace(1, 0.2, n_points),
-                np.linspace(0, 1, n_points))
-
-    # combine coordinates from different instruments
-    points = list(points_1) + list(points_2) + list(points_3)
+    # # combine coordinates from different instruments
+    # points = list(points_1) + list(points_2) + list(points_3)
     
     '''
     def plot_sunpy_maps(s_map, points):
     plt.figure()
     '''
-    
-    
+    lat = np.linspace(0, 90, n_points) # different latitudes for novel views
+    lon = np.linspace(0, 360, n_points)
+    dist = np.linspace(1, 2, n_points) 
+    time = np.zeros(n_points)  # Keep all observers at time 0
+    points = list(zip(lat, lon, dist, time))
+    print(f'points: {points}')
+
     
     
     # Read AIA image 
